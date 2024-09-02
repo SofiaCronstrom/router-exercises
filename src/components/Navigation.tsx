9bfdb7bec42fb119198ded0ee70c9126e8e12d7b
Sofia <sofia.cronstrom@gmail.com>
2024-09-02 13:59:43 +0200

    Setting up folders and files

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,11 @@
+export default function NavBar () {
+return (
+    <section>
+        <nav>
+            <a href="">Link</a>
+            <a href="">Link</a>
+            <a href="">Link</a>
+        </nav>
+    </section>
+)
+}
